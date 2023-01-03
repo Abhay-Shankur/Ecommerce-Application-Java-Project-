4249964e81c41fd2447fa8ffa1fcda42982cda11
LiveForCode <61900769+Abhay-Shankur@users.noreply.github.com>
2023-01-04 01:47:46 +0530

    Add files via upload

---
+++ Java MySQL/src/com/files/MysqlCon.java
@@ -0,0 +1,25 @@
+package com.files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.files.Connection.MyCon;
+
+public class MysqlCon {
+
+	public static void main(String args[]){  
+		try{  
+//			Class.forName("com.mysql.cj.jdbc.Driver");  
+//			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo?useSSL=false","root","Shankur@3812");  
+		//here sonoo is database name, root is username and password  
+			Connection conn =MyCon.getConnection();
+			Statement stmt=conn.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from profile");  
+			while(rs.next())  
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			conn.close();  
+		}catch(Exception e){ System.out.println(e);}  
+	}  
+
+}
